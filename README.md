66c8b063a7d42e1ec4859de5afd37c29439861ee
Ain Tohvri <at@interactive-pioneers.de>
2014-08-28 14:51:08 +0200

    Add bare Yeoman jQuery layout

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
-jquery-pbpopulator
-==================
+# jQuery Pioneer's Bower Populator
 
 jQuery Pioneer's Bower Populator syncs values between form controls.
+
+## Getting Started
+
+Download the [production version][min] or the [development version][max].
+
+[min]: https://raw.github.com/interactive-pioneers/jquery-pbpopulator/master/dist/jquery-pbpopulator.min.js
+[max]: https://raw.github.com/interactive-pioneers/jquery-pbpopulator/master/dist/jquery-pbpopulator.js
+
+In your web page:
+
+```html
+<script src="jquery.js"></script>
+<script src="dist/jquery-pbpopulator.min.js"></script>
+<script>
+jQuery(function($) {
+  // TODO example initilisation
+});
+</script>
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Release History
+_(Nothing yet)_
---
+++ src/jquery-pbpopulator.js
@@ -0,0 +1,38 @@
+/*
+ * jQuery Pioneer's Bower Populator
+ *
+ *
+ * Copyright (c) 2014 Ain Tohvri, Interactive Pioneers GmbH
+ * Licensed under the GPLv3 license.
+ */
+
+(function ($) {
+
+  // Collection method.
+  $.fn.awesome = function () {
+    return this.each(function (i) {
+      // Do something awesome to each selected element.
+      $(this).html('awesome' + i);
+    });
+  };
+
+  // Static method.
+  $.awesome = function (options) {
+    // Override default options with passed-in options.
+    options = $.extend({}, $.awesome.options, options);
+    // Return something awesome.
+    return 'awesome' + options.punctuation;
+  };
+
+  // Static method default options.
+  $.awesome.options = {
+    punctuation: '.'
+  };
+
+  // Custom selector.
+  $.expr[':'].awesome = function (elem) {
+    // Is this element awesome?
+    return $(elem).text().indexOf('awesome') !== -1;
+  };
+
+}(jQuery));
