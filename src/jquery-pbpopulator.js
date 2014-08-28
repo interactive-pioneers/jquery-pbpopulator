b58b55ff242e020f77a50b9b85204c05ba91e867
Ain Tohvri <at@interactive-pioneers.de>
2014-08-28 16:53:06 +0200

    Add populator implementation

--- src/jquery-pbpopulator.js
+++ src/jquery-pbpopulator.js
@@ -1,38 +1,142 @@
 /*
  * jQuery Pioneer's Bower Populator
  *
- *
- * Copyright (c) 2014 Ain Tohvri, Interactive Pioneers GmbH
- * Licensed under the GPLv3 license.
+ * Copyright (c) 2013-2014 Ain Tohvri, Interactive Pioneers GmbH
+ * Licensed under GPL license.
  */
 
-(function ($) {
+(function($, window, document, undefined) {
 
-  // Collection method.
-  $.fn.awesome = function () {
-    return this.each(function (i) {
-      // Do something awesome to each selected element.
-      $(this).html('awesome' + i);
+  /*
+   * Plugin UID.
+   *
+   * @type String
+   */
+  var pluginName = 'pbpopulator';
+
+  var defaults = {
+  };
+
+  function PBPopulator(element, options) {
+    this.element = element;
+    this.options = $.extend({}, defaults, options);
+    this._defaults = defaults;
+    this._name = pluginName;
+    this._init();
+  }
+
+  PBPopulator.prototype._init = function() {
+    var self = this;
+    $('*[data-population-target]').each(function(param) {
+      self._addEventListener(this);
     });
   };
 
-  // Static method.
-  $.awesome = function (options) {
-    // Override default options with passed-in options.
-    options = $.extend({}, $.awesome.options, options);
-    // Return something awesome.
-    return 'awesome' + options.punctuation;
+  PBPopulator.prototype._addEventListener = function(control) {
+    var type = this._getControlType(control);
+    switch (type) {
+      case 'input_text':
+      case 'input_password':
+      case 'input_phone':
+      case 'input_email':
+        $(control).bind('blur', this, this._populate);
+        break;
+      default:
+        $(control).bind('change', this, this._populate);
+        break;
+    }
   };
 
-  // Static method default options.
-  $.awesome.options = {
-    punctuation: '.'
+  PBPopulator.prototype._getControlType = function(control) {
+    var tag = $(control).prop('tagName').toLowerCase();
+    var type = tag;
+    if (tag === 'input') {
+      type += '_' + $(control).attr('type');
+    }
+    return type;
   };
 
-  // Custom selector.
-  $.expr[':'].awesome = function (elem) {
-    // Is this element awesome?
-    return $(elem).text().indexOf('awesome') !== -1;
+  PBPopulator.prototype._populate = function(event) {
+    var self = event.data;
+    self._populateForControl(this, $(this).attr('data-population-target'));
   };
 
-}(jQuery));
+  PBPopulator.prototype.populateOptional = function() {
+    var self = this;
+    $('*[data-population-target-optional]').each(function() {
+      self._populateForControl(this, $(this).attr('data-population-target-optional'));
+    });
+  };
+
+  PBPopulator.prototype._hasSelectMatch = function(control, value) {
+    var match = false;
+    $(control).children('option').each(function() {
+      if ($(this).val() === value.toString()) {
+        match = true;
+        return true;
+      }
+    });
+    return match;
+  };
+
+  PBPopulator.prototype._populateForControl = function(control, targetId) {
+    var $targetControl = $(this._parseId(targetId));
+    var targetType = this._getControlType($targetControl);
+    if ($targetControl.length > 0) {
+      switch (this._getControlType(control)) {
+        case 'input_checkbox':
+          switch (targetType) {
+            case 'input_hidden':
+              if ($(control).prop('checked')) {
+                $targetControl.val(1).prop('disabled', false);
+              }
+              else {
+                $targetControl.prop('disabled', true).removeAttr('value');
+              }
+              break;
+            case 'input_checkbox':
+              $targetControl.prop('checked', $(control).prop('checked'));
+              break;
+            default:
+              $targetControl.val($(control).val());
+              break;
+          }
+          break;
+        case 'select':
+          var targetedValue = $(control).val();
+          if (this._hasSelectMatch($targetControl, targetedValue)) {
+            $targetControl.val(targetedValue);
+            $(this.element).trigger('success', { type: 'populationSuccess', target: $targetControl, source: $(control) });
+          }
+          else {
+            var errorDefaultValue = $targetControl.data('population-error-default-value');
+            if (errorDefaultValue && this._hasSelectMatch($targetControl, errorDefaultValue)) {
+              $targetControl.val(errorDefaultValue);
+            }
+            $(this.element).trigger('error', { type: 'populationMismatch', target: $targetControl, source: $(control) });
+          }
+          break;
+        default:
+          $targetControl.val($(control).val());
+          break;
+      }
+      $targetControl.trigger('change', $(control));
+    }
+  };
+
+  PBPopulator.prototype._parseId = function(str) {
+    var ids = str.replace(/\s+/g, '').split(',');
+    for (var i = 0, l = ids.length; i < l; i++) {
+      ids[i] = "#" + ids[i];
+    }
+    return ids.join(',');
+  };
+
+  $.fn[pluginName] = function(options) {
+    return this.each(function() {
+      if (!$.data(this, 'plugin_' + pluginName)) {
+        $.data(this, 'plugin_' + pluginName, new PBPopulator(this, options));
+      }
+    });
+  };
+})(jQuery, window, document);
